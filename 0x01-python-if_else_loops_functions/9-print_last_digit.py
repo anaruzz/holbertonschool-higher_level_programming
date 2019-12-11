c86220c2710e049c139567cc70f35831012659e2
anaruzz <laabidigh@gmail.com>
2019-12-11 15:22:13 +0100

    Task 9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def print_last_digit(number):
+    if number > 0:
+        var = number % 10
+    elif number < 0:
+        var = (-number) % 10
+    else:
+        var = 0
+    print("{:d}".format(var), end="")
+    return (var)
