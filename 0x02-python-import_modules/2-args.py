c5b8089dead58e503d6ca69642154895be326fcd
anaruzz <laabidigh@gmail.com>
2019-12-12 22:18:51 +0100

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 import sys
-l= len(sys.argv)
+argv = sys.argv
+l= len(argv)
+
 if l > 1:
     if l == 2:
         print("{:d} argument:".format(l - 1))
-        print("{:d}: {:s}".format(1, sys.argv[1]))
+        print("{:d}: {:s}".format(1, argv[1]))
     else:
         print("{:d} arguments:".format(l - 1))
         for i in range(1, l):
-            print("{:d}: {:s}".format(i, sys.argv[i]))
+            print("{:d}: {:s}".format(i, argv[i]))
 else:
     print("{:d} arguments.".format(l - 1))
