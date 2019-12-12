11bc22b5bfc11ce39fc739417881dfafa98b3e86
anaruzz <laabidigh@gmail.com>
2019-12-12 23:53:11 +0100

    Fix adv 1

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,24 +2,23 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
+argv = sys.argv
+len = len(argv)
 
-l = len(sys.argv)
-print(l)
-if l != 4:
+if len != 4:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(0)
-
-a = int(sys.argv[1])
-b = int(sys.argv[3])
-op = sys.argv[2]
-if op == '+':
-    print("{:d} {} {:d} = {:d}".format(a, op, b, add(a, b)))
-elif op == '-':
-    print("{:d} {} {:d} = {:d}".format(a, op, b, sub(a, b)))
-elif op == '*':
-    print("{:d} {} {:d} = {:d}".format(a, op, b, mul(a, b)))
-elif op == '/':
-    print("{:d} {} {:d} = {:d}".format(a, op, b, div(a, b)))
-else:
-    print("Unknown operator. Available operators: +, -, * and / ")
     sys.exit(1)
+a = int(argv[1])
+b = int(argv[3])
+op = argv[2]
+if op != "+" and op != "-" and op != "*" and op != "/":
+    print("Unknown operator. Available operators: +, -, * and /")
+    sys.exit(1)
+if op == "+":
+    print("{:d} {:s} {:d} = {:d}".format(a, op, b, add(a, b)))
+if op == "-":
+    print("{:d} {:s} {:d} = {:d}".format(a, op, b, sub(a, b)))
+if op == "*":
+    print("{:d} {:s} {:d} = {:d}".format(a, op, b, mul(a, b)))
+if op == "/":
+    print("{:d} {:s} {:d} = {:d}".format(a, op, b, div(a, b)))
