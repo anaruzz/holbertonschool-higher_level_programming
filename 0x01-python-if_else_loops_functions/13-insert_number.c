75c9e5a6d9af5d48c290f65315fbc30f8095f860
anaruzz <laabidigh@gmail.com>
2019-12-25 02:33:15 +0100

    Insert in sorted list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_node- insert node in a sorted list
+ * @head: head of node
+ * @number: value of node to be inserted
+ * Return: node
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+listint_t *new, *h = *head;
+
+new = malloc(sizeof(listint_t));
+if (!new)
+return (NULL);
+
+if (!*head)
+return (NULL);
+
+new->n = number;
+while(h)
+{
+h = h->next;
+if ((h->next)->n > number)
+break;
+}
+
+if (h->next != NULL)
+{
+new->next = h->next;
+h->next = new;
+}
+else
+{
+  h->next = new;
+  new->next = NULL;
+}
+return (h);
+}
