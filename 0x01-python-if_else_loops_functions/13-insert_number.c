3292892a47ad3d9cf8010031d8cef071aeebc5a9
anaruzz <laabidigh@gmail.com>
2019-12-25 03:54:16 +0100

    try again

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,6 +1,4 @@
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
@@ -11,35 +9,47 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-listint_t *new, *h = *head;
+  listint_t *new, *h = *head;
 
-new = malloc(sizeof(listint_t));
-if (!new)
-return (NULL);
+  new = malloc(sizeof(listint_t));
+  if (!new)
+  return (NULL);
 
-if (!*head)
-{
-(*head)->n = number;
-(*head)->next = NULL;
-}
-new->n = number;
-while(h)
-{
-h = h->next;
-if ((h->next)->n > number)
-break;
-}
+  new->n = number ;
 
-if (h->next != NULL)
-{
-new->next = h->next;
-h->next = new;
-}
-else
-{
-  h->next = new;
+  if (*head == NULL)
+  {
   new->next = NULL;
-}
-h = new;
-return (h);
+  *head = new;
+  return (*head);
+  }
+
+  if (number < h->n)
+  {
+    new->next = h->next;
+    *head = new;
+    return(new);
+  }
+  while (h)
+  {
+    if (number > (h->next)->n)
+    h = h->next;
+    else
+    break;
+  }
+  if (h->next == NULL && number > h->n)
+  {
+    h->next = new;
+    new->next = NULL;
+    h = new;
+    return (h);
+  }
+  if (h->next != NULL)
+  {
+    new->next = h->next;
+    h->next = new;
+    h = new;
+    return (h);
+  }
+return (NULL);
 }
