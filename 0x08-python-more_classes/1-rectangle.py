4e519732aa153749736956291246dd5649cfed88
anaruzz <laabidigh@gmail.com>
2020-01-18 01:51:22 +0100

    Fix 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,39 +1,29 @@
 #!/usr/bin/python3
 class Rectangle:
     def __init__(self, width=0, height=0):
-        if height is not None:
-            if type(height) is not int:
-                raise TypeError("height must be integer")
-            if height < 0:
-                raise ValueError("height must be >= 0")
-            self.__height = height
-        if width is not None:
-            if type(width) is not int:
-                raise TypeError("width must be integer")
-            if width < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = width
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
-        return self.__width
+        return (self.__width)
 
     @width.setter
     def width(self, value):
-        if type(value) is not int:
-            raise TypeError("width must be integer")
-        if value < 0:
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if int(value) < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
-        return self.__height
+        return (self.__height)
 
     @height.setter
     def height(self, value):
-        if type(value) is not int:
-            raise TypeError("height must be integer")
-        if value < 0:
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if int(value) < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
