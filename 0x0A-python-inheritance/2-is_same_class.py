17114f2f8c26bb8cfc2c18fd547ee3b3af6666c9
anaruzz <laabidigh@gmail.com>
2020-01-21 20:29:35 +0100

    Simpler answer

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def is_same_class(obj, a_class):
     """
-    return True if type of obj is a subclass of a_class else False
+    return True if type of obj is a subcl of a_class else False
     """
-    return issubclass(a_class, type(obj))
+    return type(obj) is a_class
