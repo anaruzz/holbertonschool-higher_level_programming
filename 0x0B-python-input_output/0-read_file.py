e718b7e6276fface8003e42806b75c882a3548b9
anaruzz <laabidigh@gmail.com>
2020-01-22 18:00:21 +0100

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,6 @@ def read_file(filename=""):
     function that reads a file and prints
     the content to output
     """
-    for line in open(filename):
-        print(line, end="")
+    with open(filename) as f:
+        for line in f:
+            print(line, end="")
