1a417b9ab8870e33aee29ee0f8bbe6875e255171
anaruzz <laabidigh@gmail.com>
2020-01-22 23:01:14 +0100

    Task 13 seriously ghof?

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+class Student:
+    """
+    class student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        selected_a = {}
+        for existing_a, value in self.__dict__.items():
+            for requested_a in attrs:
+                if requested_a is existing_a:
+                    selected_a[existing_a] = value
+        return selected_a
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            self.__dict__[key] = value
