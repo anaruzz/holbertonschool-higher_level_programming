9d66f559293c76c19e51e4692ad6a5cb35a25f6d
anaruzz <laabidigh@gmail.com>
2020-01-22 23:35:58 +0100

    Task 14 Done!

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    line = []
+    for i in range(n):
+        row = []
+        line.append(row)
+        for j in range(0, i + 1):
+            if j == 0 or j is i:
+                line[i].append(1)
+            else:
+                line[i].append(line[i -1][j -1] + line[i -1][j])
+    return line
