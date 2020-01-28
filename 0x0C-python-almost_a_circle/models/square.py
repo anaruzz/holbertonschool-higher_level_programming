dae19cf00acd5dee90c815d6005ccf1fbfb9e829
anaruzz <laabidigh@gmail.com>
2020-01-28 23:12:24 +0100

    Task 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,8 +8,8 @@ class Square(Rectangle):
     Class Square that inherits from class Rectangle
     """
     def __init__(self, size, x=0, y=0, id=None):
-        """constructor for  class Square"""
-        Rectangle.__init__(size, size, x, y, id)
+        """ constructor for  class Square """
+        Rectangle.__init__(self, size, size, x, y, id)
         self.size = size
 
     def __str__(self):
