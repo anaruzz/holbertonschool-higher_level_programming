e4f4c188d6f96ce719eb8fcdb359d6eb5bb3a221
anaruzz <laabidigh@gmail.com>
2020-03-29 17:13:32 +0100

    Task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# Script that lists all states from a database
+
+if __name__ == "__main__":
+
+    from sys import argv
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    state_name = argv[4]
+
+    if ';' in state_name:
+        print('SQL injection failed')
+        exit()
+
+    import MySQLdb
+    db = MySQLdb.connect(host='localhost', user=username, port=3306,
+                         passwd=password, db=database)
+
+    cur = db.cursor()
+    sql = "SELECT * FROM states WHERE name = '{}'\
+            ORDER BY id ASC".format(state_name)
+    cur.execute(sql)
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1] == state_name:
+            print("{}".format(row))
