6560b7e46e6d663ac4326a29fc44665f6321cd22
anaruzz <laabidigh@gmail.com>
2020-03-30 18:37:06 +0100

    List all object from the table states

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,10 +8,7 @@ import sys
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-def main(argv):
-    """
-    main function
-    """
+if __name__ == '__main__' and len(sys.argv) == 4:
     engine = create_engine('mysql+mysqldb://{}:{}\@localhost/{}'.format(
                             sys.argv[1],
                             sys.argv[2],
@@ -19,13 +16,8 @@ def main(argv):
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    Session = sessionmaker()
-    Session.configure(bind=engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
 
-    for instance in session.query(State).order_by(State.id):
+    for instance in session.query(State).order_by(State.id).all():
         print("{}: {}".format(instance.id, instance.name))
-
-
-if __name__ == '__main__' and len(sys.argv) == 4:
-    main(sys.argv)
