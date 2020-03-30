f8a7a494e3134331c2df77d8ae75d8a756275caf
anaruzz <laabidigh@gmail.com>
2020-03-30 19:11:41 +0100

    List first Object

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module that lists all State objects
+"""
+
+from model_state import Base, State
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == '__main__' and len(sys.argv) == 4:
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                            sys.argv[1],
+                            sys.argv[2],
+                            sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    instance = session.query(State).first()
+    print("{}: {}".format(instance.id, instance.name))
