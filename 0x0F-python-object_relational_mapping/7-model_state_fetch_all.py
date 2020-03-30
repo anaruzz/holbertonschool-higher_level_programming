f29488e4e8964cac9046bd2bd1f8d91ff4d1ed18
anaruzz <laabidigh@gmail.com>
2020-03-30 19:06:34 +0100

    Task 8

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python3
 """
-Script that lists all State objects from the database hbtn_0e_6_usa.
+Module that lists all State objects
 """
 
-
-import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
+import sys
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
 
-
-def list_states():
-    """List all the states from the hbtn_0e_6_usa database."""
-
-    arg = sys.argv
-    url_base = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
-    db_url = url_base.format(arg[1], arg[2], arg[3])
-    engine = create_engine(db_url, pool_pre_ping=True)
+if __name__ == '__main__' and len(sys.argv) == 4:
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                            sys.argv[1],
+                            sys.argv[2],
+                            sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    for inst in session.query(State).order_by(State.id):
-        print(inst.id, ": ", inst.name, sep="")
 
-
-if __name__ == "__main__":
-    list_states()
+    for instance in session.query(State).order_by(State.id):
+        print("{}: {}".format(instance.id, instance.name))
