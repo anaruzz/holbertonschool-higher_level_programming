e2bc7eea0cf92d3593a5aca5631db17b382c5f57
anaruzz <laabidigh@gmail.com>
2020-03-31 15:33:27 +0100

    pep8 Fix

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -19,6 +19,6 @@ if __name__ == '__main__' and len(sys.argv) == 4:
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    query = session.query(State).filter(State.id=='2').first()
+    query = session.query(State).filter(State.id == '2').first()
     query.name = 'New Mexico'
     session.commit()
