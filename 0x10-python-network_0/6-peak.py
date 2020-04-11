08834a4163c3d3aa73284ea8bdf46c823ad0ba3a
anaruzz <laabidigh@gmail.com>
2020-04-11 03:35:38 +0100

    peak Fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,9 +6,10 @@ def find_peak(list_of_integers):
     find the peak element in a list
     """
     arr = list_of_integers
-    if arr:
-        for i in range(1, len(arr)-1):
-            if arr[i] > arr[i-1] and arr[i] > arr[i+1]:
-                return arr[i]
-        return max(arr[0], arr[-1])
-    return None
+    if arr = []:
+        return None
+
+    for i in range(1, len(arr)-1):
+        if arr[i] > arr[i-1] and arr[i] > arr[i+1]:
+            return arr[i]
+    return max(arr[0], arr[-1])
