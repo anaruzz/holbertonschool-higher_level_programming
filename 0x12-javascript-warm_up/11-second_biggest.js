669c6538ce701b1f5d008acab415d56d56ee9124
anaruzz <laabidigh@gmail.com>
2020-05-05 14:18:08 +0100

    ok

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,22 +3,18 @@
 if (process.argv.length < 3) {
   console.log('0');
 } else {
-  if (process.argv.length === 3) {
-    console.log('0');
-  } else {
-    let i = 3; let current;
-    let max1 = parseInt(process.argv[2]);
-    let max2 = parseInt(process.argv[2]);
-    for (; i < process.argv.length; i++) {
-      current = parseInt(process.argv[i]);
-      if (current > max1) {
-        max2 = max1;
-        max1 = current;
-      }
-      if (current > max2 && current < max1) {
-        max2 = current;
-      }
+  let i = 3; let current;
+  let max1 = parseInt(process.argv[2]);
+  let max2 = parseInt(process.argv[2]);
+  for (; i < process.argv.length; i++) {
+    current = parseInt(process.argv[i]);
+    if (current > max1) {
+      max2 = max1;
+      max1 = current;
     }
-    console.log(max2);
+    if (current > max2 && current < max1) {
+      max2 = current;
+    }
   }
+  console.log(max2);
 }
