b172199363999d3463ad65c9e368dad4e13d3869
anaruzz <laabidigh@gmail.com>
2020-05-05 13:44:00 +0100

    Fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,8 +3,8 @@
 if (!parseInt(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-    let i = 0;
-    for (; i < parseInt(process.argv[2]); i++) {
-      console.log('C is fun');
-    }
+  let i = 0;
+  for (; i < parseInt(process.argv[2]); i++) {
+    console.log('C is fun');
   }
+}
