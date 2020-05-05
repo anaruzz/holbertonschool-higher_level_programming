2cc21b61eb2c15bcc3aeb8850f614cb647a229e2
anaruzz <laabidigh@gmail.com>
2020-05-05 12:15:42 +0100

    Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// a script that adds the first two arguments
+function factorial (a) {
+  if (isNaN(a)) {
+    return 1;
+  }
+  if (a === 1) {
+    return 1;
+  }
+  return a * factorial(a - 1);
+}
+const n = parseInt(process.argv[2]);
+console.log(factorial(n));
