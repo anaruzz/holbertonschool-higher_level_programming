661e864e62149c32f6a0c4c3fa1545089d20fdde
anaruzz <laabidigh@gmail.com>
2020-05-05 13:43:23 +0100

    Fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,15 +1,10 @@
 #!/usr/bin/node
 // a script that prints x times “C is fun”
-if (process.argv.length < 3) {
+if (!parseInt(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-  const a = parseInt(process.argv[2]);
-  if (isNaN(a)) {
-    console.log('Missing number of occurrences');
-  } else {
     let i = 0;
-    for (; i < a; i++) {
+    for (; i < parseInt(process.argv[2]); i++) {
       console.log('C is fun');
     }
   }
-}
