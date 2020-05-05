e3d02e820f3da6c8fe7927f107e3356793a70bf2
anaruzz <laabidigh@gmail.com>
2020-05-05 11:28:05 +0100

    An Integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// a script that prints two arguments passed to it in a format
+if (process.argv.length < 3) {
+  console.log('Not a number');
+} else {
+  const a = parseInt(process.argv[2]);
+  if (isNaN(a)) {
+    console.log('Not a number');
+  } else {
+    console.log(a);
+  }
+}
