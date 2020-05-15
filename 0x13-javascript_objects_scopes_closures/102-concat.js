7bf3182cbd05f9cdbc01ea14f88a60d64507b775
anaruzz <laabidigh@gmail.com>
2020-05-16 00:05:43 +0100

    semistandard

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const file_1 = fs.readFileSync(process.argv[2], 'utf8');
-const file_2 = fs.readFileSync(process.argv[3], 'utf8');
+const fileOne = fs.readFileSync(process.argv[2], 'utf8');
+const fileTwo = fs.readFileSync(process.argv[3], 'utf8');
 
-const all_content = file_1 + file_2;
+const allContent = fileOne + fileTwo;
 
-fs.appendFileSync(process.argv[4], all_content);
+fs.appendFileSync(process.argv[4], allContent);
