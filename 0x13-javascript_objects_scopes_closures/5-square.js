d79355f2e19dd81090c743a4ddbc8b29c5861cae
anaruzz <laabidigh@gmail.com>
2020-05-15 23:18:07 +0100

    square

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Script that creates square
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size)
+  }
+}
+module.exports = Square;
