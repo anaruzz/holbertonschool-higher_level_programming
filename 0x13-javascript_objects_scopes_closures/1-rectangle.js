9c7147c13d8737f9c6b5eb3a8196d5265f67947c
anaruzz <laabidigh@gmail.com>
2020-05-15 22:54:01 +0100

    undefined

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -2,9 +2,11 @@
 // Script that creates empty class
 
 class Rectangle {
-  constructor (h, w) {
+  constructor (w, h) {
+    this.width = undefined;
+    this.height = undefined;
     this.width = w;
     this.height = h;
-  }
+    }
 }
 module.exports = Rectangle;
