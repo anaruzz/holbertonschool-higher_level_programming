d8d5332fbca2aa296d1d443ea5b58bd756c9294a
anaruzz <laabidigh@gmail.com>
2020-05-17 19:25:26 +0100

    Task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Script that reads a file and prints its content
+
+const fs = require('fs');
+const file = process.argv[2];
+const encoding = 'utf-8';
+
+fs.readFile(file, encoding, function(err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
